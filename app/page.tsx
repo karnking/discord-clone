a6f36e49a6ff9b7cffa9fbeffdb2fe0f4de18679
karnking <karankotai9183@gmail.com>
2023-09-13 23:36:10 +0530

    authenticating

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return <>
-    <p className='text-5xl font-bold text-indigo-500'>Hello</p>
-    <Button>Add</Button>
-  </>
-}
