3e983ba59756f99a72c63413b9669d2794e725b0
karnking <karankotai9183@gmail.com>
2023-09-20 23:59:04 +0530

    upload thing routes

--- app/api/uploadThing/route.ts
+++ app/api/uploadThing/route.ts
@@ -2,7 +2,7 @@ import { createNextRouteHandler } from "uploadthing/next";
  
 import { ourFileRouter } from "./core";
  
-// Export routes for Next App Router
+// Export routes for Next App Route
 export const { GET, POST } = createNextRouteHandler({
   router: ourFileRouter,
 });
