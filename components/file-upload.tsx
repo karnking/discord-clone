70342e106cf8ad8bd924002acb3cbeb4cdd7af3b
karnking <karankotai9183@gmail.com>
2023-09-21 23:35:36 +0530

    component made

---
+++ components/file-upload.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export const FileUpload = () => {
+    return (
+        <div>
+            FIle upload content
+        </div>
+    )
+}
--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -23,6 +23,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { useEffect, useState } from "react";
+import { FileUpload } from "../file-upload";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -47,11 +48,11 @@ export const InitialModal = () => {
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         console.log(values)
     }
-    const [isMounted,setIsMounted] = useState(false)
-    useEffect(()=>{
+    const [isMounted, setIsMounted] = useState(false)
+    useEffect(() => {
         setIsMounted(true)
-    },[])
-    if(!isMounted) return null
+    }, [])
+    if (!isMounted) return null
     return (
         <Dialog open>
             <DialogContent className='bg-white text-black p-0 overflow-hidden'>
@@ -66,19 +67,31 @@ export const InitialModal = () => {
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                         <div className="space-y-8 px-6">
-                            <div className="flex items-center justify-center text-center">TODO: Image Upload</div>
+                            <div className="flex items-center justify-center text-center">
+                                <FormField
+                                    control={form.control}
+                                    name="imageUrl"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
                             <FormField control={form.control} name="name" render={({ field }) => (
                                 <FormItem>
                                     <FormLabel className="uppercase text=xs font-bold text-zinc-500 dark:text-secondary/70">Server Name</FormLabel>
                                     <FormControl>
-                                        <Input 
+                                        <Input
                                             disabled={isLoading}
-                                            className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0" 
+                                            className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
                                             placeholder="Enter server name"
                                             {...field}
                                         />
                                     </FormControl>
-                                    <FormMessage/>
+                                    <FormMessage />
                                 </FormItem>
                             )} />
                         </div>
