880c175458ce881bc403cc80bbed2addf16956f1
karnking <karankotai9183@gmail.com>
2023-09-22 23:58:16 +0530

    new routes added

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -14,7 +14,7 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
     if(value && fileType !=="pdf"){
         return (
             <div className="relative h-20 w-20">
-                <Image fill src={value} alt='upload' className="rounded-half" />
+                <Image fill src={value} alt='upload' className="rounded-full" />
             </div>
         )
     }
