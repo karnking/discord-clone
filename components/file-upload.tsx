7af5211a5f536a7cd6357e0f307307ed3b71a8f8
karnking <karankotai9183@gmail.com>
2023-09-22 23:53:04 +0530

    next config for upload things

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,5 +1,6 @@
 "use client"
-
+import { X } from "lucide-react";
+import Image from "next/image"
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css"
 
@@ -9,6 +10,14 @@ interface FileUploadProps {
     endpoint: "messageFile" | "serverImage"
 }
 export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+    const fileType = value?.split(".").pop()
+    if(value && fileType !=="pdf"){
+        return (
+            <div className="relative h-20 w-20">
+                <Image fill src={value} alt='upload' className="rounded-full" />
+            </div>
+        )
+    }
     return (
         <UploadDropzone
             endpoint={endpoint}
