71ab0882e0c35c7a9587081644c3c06a31c3167c
karnking <karankotai9183@gmail.com>
2023-09-23 23:52:16 +0530

    new line

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -8,7 +8,7 @@ const SetupPage = async() => {
         where:{
             members: {
                 some: {
-                    profileId: profile.i
+                    profileId: profile.id
                 }
             }
         }
