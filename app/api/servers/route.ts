19cce5a4ed644bfb025bb31c171326f909d98fc8
karnking <karankotai9183@gmail.com>
2023-09-24 23:53:49 +0530

    server creation successfull

--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -32,7 +32,7 @@ export async function POST(req: Request) {
         }
       }
     });
-
+    console.log(server)
     return NextResponse.json(server);
   } catch (error) {
     console.log("[SERVERS_POST]", error);
--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -57,8 +57,7 @@ export const InitialModal = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      // await axios.post("/api/servers", values);
-      console.log(values)
+      await axios.post("/api/servers", values);
       form.reset();
       router.refresh();
       window.location.reload();
