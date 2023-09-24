f3a05b9aa1d896840a4e19f2abe58d99ec963c4e
karnking <karankotai9183@gmail.com>
2023-09-24 23:16:00 +0530

    profile uitil made

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -54,7 +54,7 @@ export const FileUpload = ({
         </a>
         <button
           onClick={() => onChange("")}
-          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+          className="bg-rose-400 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
           type="button"
         >
           <X className="h-4 w-4" />
