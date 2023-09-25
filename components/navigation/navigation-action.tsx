967a7a02c5527b9bc5eecc041fac462a94078750
karnking <karankotai9183@gmail.com>
2023-09-26 00:01:53 +0530

    Plus action made

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import { Plus } from "lucide-react";
+
 export const NavigationAction = () => {
     return (
         <div>
-            action
+            <button>
+                <div className="flex mx-3 h-[48px] w-[48px] rounted-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
+                    <Plus className="group-hover:text-white transition text-emerald-500" size={25} />
+                </div>
+            </button>
         </div>
     )
 }
