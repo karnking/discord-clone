712ae3deb3cb4c6286f50d6cce423ea0c4b9d434
karnking <karankotai9183@gmail.com>
2023-09-25 23:44:20 +0530

    set default dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <body className={cn(
             font.className,"bg-white dark:bg-[#313338]"
           )}>
-          <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false} storageKey='discord-theme' >
+          <ThemeProvider attribute='class' defaultTheme='dark' forcedTheme='dark' enableSystem={false} storageKey='discord-theme' >
             {children}
           </ThemeProvider>
         </body>
