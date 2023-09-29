337739cc6f4f78262a6d55191dbe6fd304494993
karnking <karankotai9183@gmail.com>
2023-09-29 16:30:51 +0530

    server navigation sidebar done

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -4,6 +4,7 @@ import {db} from "@/lib/db"
 import { NavigationAction } from "./navigation-action";
 import { Separator } from "@/components/ui/separator";
 import {ScrollArea} from "@/components/ui/scroll-area"
+import { NavigationItem } from "./navigation-item";
 
 export const NavigationSidebar = async () => {
     const profile = await currentProfile();
@@ -26,7 +27,13 @@ export const NavigationSidebar = async () => {
         <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] bg-[#E3E5E8] py-3">
             <NavigationAction />
             <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md v-10 mx-auto"/>
-            <ScrollArea />
+            <ScrollArea className="flex-1 w-full">
+                {servers.map((server)=>{
+                    return <div key={server.id}>
+                        <NavigationItem {...server}/>
+                    </div>
+                })}
+            </ScrollArea>
         </div>
     )
 }
