0c54609a07aacc65f953f399e48cc0bf87f5c95a
karnking <karankotai9183@gmail.com>
2023-09-30 00:00:15 +0530

    checking size

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -10,7 +10,7 @@ export const NavigationAction = () => {
             <ActionTooltip side='right' align='center' label='Add a server'>
                 <button className="group flex items-center" onClick={()=>onOpen("createServer")}>
                     <div className="flex mx-3 h-[48px] w-[48px] rounted-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
-                        <Plus className="group-hover:text-white transition text-emerald-500" size={2} />
+                        <Plus className="group-hover:text-white transition text-emerald-500" size={25} />
                     </div>
                 </button>
             </ActionTooltip>
