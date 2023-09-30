d5a4c7ec9cecf550cf4ede049c077932f0f36c7e
karnking <karankotai9183@gmail.com>
2023-09-30 23:19:58 +0530

    invite copy functionality done

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -10,13 +10,30 @@ import { useModal } from "@/hooks/use-modal-store";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Copy, RefreshCw } from "lucide-react";
+import { Check, Copy, RefreshCw } from "lucide-react";
+import { useOrigin } from "@/hooks/use-origin";
+import { useState } from "react";
 
 
 export const InviteModal = () => {
-  const {isOpen,onClose,type} = useModal();
+  const {isOpen,onClose,type, data} = useModal();
+  const origin = useOrigin();
 
   const isModalOpen = isOpen && type==="invite"
+  const {server} = data;
+
+  const [copied,setCopied] = useState(false);
+  const [isLoading,setIsLoading] = useState(false);
+
+  const inviteUrl =  `${origin}/invite/${server?.inviteCode}`;
+
+  const onCopy = ( ) => {
+    navigator.clipboard.writeText(inviteUrl)
+    setCopied(true)
+    setTimeout(()=>{
+      setCopied(false)
+    },1000)
+  }
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -31,9 +48,12 @@ export const InviteModal = () => {
             Server invite link
           </Label>
           <div className="flex items-center mt-2 gap-x-2">
-            <Input className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus visible:ring-offset-0" value={"invite-link"}/>
-            <Button size="icon">
-              <Copy className="w-5 h-4"/>
+            <Input className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus visible:ring-offset-0" value={inviteUrl}/>
+            <Button size="icon" onClick={onCopy}>
+              {copied ? 
+                <Check className="w-5 h-4"/> :
+                <Copy className="w-5 h-4"/>
+              }
             </Button>
           </div>
             <Button variant="link" size='sm' className="text-xs text-zinc-500 mt-4">
