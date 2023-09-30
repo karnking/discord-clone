661622c060de35276fd683fb5f2d12b69d93878e
karnking <karankotai9183@gmail.com>
2023-09-30 17:31:29 +0530

    dropdown trigger made

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -2,19 +2,35 @@
 
 import { ServerWithMembersWithProfile } from "@/types"
 import { MemberRole } from "@prisma/client";
+import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { ChevronDown } from "lucide-react";
 
 interface ServerHeaderProps {
     server: ServerWithMembersWithProfile;
     role?: MemberRole;
-}; 
+};
 
 export const ServerHeader = ({
     server,
     role
 }: ServerHeaderProps) => {
+
+    const isAdmin = role === MemberRole.ADMIN;
+    const isModerator = isAdmin || role === MemberRole.MODERATOR
+
     return (
-        <div>
-            Server Header
-        </div>
+        <DropdownMenu>
+            <DropdownMenuTrigger
+                className="focus:outline-none"
+                asChild
+            >
+                <button 
+                    className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
+                >
+                    {server.name}
+                    <ChevronDown className="h-5 w-5 ml-auto"/>
+                </button>
+            </DropdownMenuTrigger>
+        </DropdownMenu>
     )
 }
