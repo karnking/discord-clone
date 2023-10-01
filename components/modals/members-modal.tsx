fd036b56bd205f79066cb748d74fb6f358ae5b03
karnking <karankotai9183@gmail.com>
2023-10-01 23:58:04 +0530

    trying member

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -9,7 +9,8 @@ import {
 } from "@/components/ui/dialog";
 import { useModal } from "@/hooks/use-modal-store";
 import { ServerWithMembersWithProfile } from "@/types";
-import { ScrollArea } from "../ui/scroll-area";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { UserAvatar } from "@/components/user-avatar";
 
 
 export const MembersModal = () => {
@@ -32,9 +33,9 @@ export const MembersModal = () => {
         <ScrollArea className="mt-8 max-h-[420px] pr-6">
           {server?.members?.map((member)=>(
             <div key={member.id} className="flex items-center gap-x-2 mb-6">
-              <UserAvatar />
+              <UserAvatar src={member.imageUrl} />
             </div>
-          )}
+          ))}
         </ScrollArea>
       </DialogContent>
     </Dialog>
