67f07f5860aa2fd54d5f5f301ae054f3b6c90be4
karnking <karankotai9183@gmail.com>
2023-10-02 23:53:14 +0530

    direct calling

--- components/modals/leave-server-modal.tsx
+++ components/modals/leave-server-modal.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import axios from "axios";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 import {
   Dialog,
   DialogContent,
@@ -11,30 +14,32 @@ import {
 } from "@/components/ui/dialog";
 import { useModal } from "@/hooks/use-modal-store";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
 
 export const LeaveServerModal = () => {
-  const router = useRouter()
   const { isOpen, onClose, type, data } = useModal();
+  const router = useRouter();
 
-  const isModalOpen = isOpen && type === "leaveServer"
+  const isModalOpen = isOpen && type === "leaveServer";
   const { server } = data;
 
   const [isLoading, setIsLoading] = useState(false);
-  
-  const onClick = async() => {
-    try{
-      setIsLoading(true)
-      await axios.patch(`/api/servers/${server?.id}/leave`)
-      router.refresh()
-      router.push('/')
-    }catch(error){
-      console.log(error)
-    }finally{
-      setIsLoading(false)
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      await axios.patch(`/api/servers/${server?.id}/leave`);
+
+      onClose();
+      router.refresh();
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   }
+
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
@@ -43,14 +48,14 @@ export const LeaveServerModal = () => {
             Leave Server
           </DialogTitle>
           <DialogDescription className="text-center text-zinc-500">
-            Are you sure you want to leave <span className="font-semibold text-indigo-500">{server?.name}</span>
+            Are you sure you want to leave <span className="font-semibold text-indigo-500">{server?.name}</span>?
           </DialogDescription>
         </DialogHeader>
         <DialogFooter className="bg-gray-100 px-6 py-4">
           <div className="flex items-center justify-between w-full">
             <Button
               disabled={isLoading}
-              onClick={()=>onClose()}
+              onClick={onClose}
               variant="ghost"
             >
               Cancel
@@ -58,7 +63,7 @@ export const LeaveServerModal = () => {
             <Button
               disabled={isLoading}
               variant="primary"
-              onClick={() => {onClick}}
+              onClick={onClick}
             >
               Confirm
             </Button>
