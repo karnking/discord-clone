584e5d8a89a3576bf87b7e71965f62bda380e5cf
karnking <karankotai9183@gmail.com>
2023-10-02 23:15:03 +0530

    on submit edited

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import qs from "query-string"
 import axios from "axios";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -22,7 +23,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { useModal } from "@/hooks/use-modal-store";
 import { ChannelType } from "@prisma/client";
 
@@ -49,7 +50,7 @@ const formSchema = z.object({
 export const CreateChannellModal = () => {
   const { isOpen, onClose, type } = useModal();
   const router = useRouter();
-
+  const params = useParams()
   const isModalOpen = isOpen && type === "createChannel"
 
   const form = useForm({
@@ -64,7 +65,13 @@ export const CreateChannellModal = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      await axios.post("/api/servers", values);
+      const url = qs.stringifyUrl({
+        url: "/api/channels",
+        query: {
+          serverId: params?.serverId
+        }
+      })
+      await axios.post(url, values);
       form.reset();
       router.refresh();
       onClose()
