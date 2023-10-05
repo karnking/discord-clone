742fc9f71bd50a247f513534719f1bd695d7712c
karnking <karankotai9183@gmail.com>
2023-10-05 23:40:22 +0530

    fixing default create channel modals

--- components/server/server-section.tsx
+++ components/server/server-section.tsx
@@ -29,7 +29,7 @@ export const ServerSection  = ({
             </p>
             {role!==MemberRole.GUEST && sectionType === 'channels' && (
                 <ActionTooltip label="Create Channel" side='top'>
-                    <button className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" onClick={()=>onOpen("createChannel")}>
+                    <button className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" onClick={()=>onOpen("createChannel",{channelType})}>
                         <Plus className="h-4 w-4" />
                     </button>
                 </ActionTooltip>
