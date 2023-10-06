ee07ef258194ef05ec0769a390fe992272f91641
karnking <karankotai9183@gmail.com>
2023-10-06 23:41:38 +0530

    edit modal and api done

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -10,6 +10,7 @@ import { CreateChannellModal } from "@/components/modals/create-channel-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
+import { EditChannellModal } from "@/components/modals/edit-channel-modal";
 
 export const ModalProvider = () => {
     const [isMounted,setIsMounted] = useState(false);
@@ -26,6 +27,7 @@ export const ModalProvider = () => {
             <LeaveServerModal />
             <DeleteServerModal />
             <DeleteChannelModal />
+            <EditChannellModal />
         </>
     )
 }
