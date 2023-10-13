7bb52dc279856805d31554fbf51cd8b58cf18716
karnking <karankotai9183@gmail.com>
2023-10-13 23:58:04 +0530

    message api done and simple messages showing

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,9 +1,16 @@
 "use client"
 
-import { Member } from "@prisma/client";
+import { Member, Message, Profile } from "@prisma/client";
 import { ChatWelcome } from "./chat-welcome";
 import { useChatQUery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
+import { Fragment } from "react";
+
+type MessageWithMemberWithProfile = Message & {
+    member: Member & {
+        profile: Profile
+    }
+}
 
 interface ChatMessagesProps {
     name: string;
@@ -55,7 +62,17 @@ export const ChatMessages = ({
         <div className="flex-1 flex flex-col py-4 overflow-y-auto">
             <div className="flex-1"/>
             <ChatWelcome type={type} name={name}/>
-            {}
+            <div className="flex flex-col-reverse mt-auto">
+                {data?.pages?.map((group,i)=> (
+                    <Fragment key={i}>
+                        {group.items.map((message:MessageWithMemberWithProfile)=>(
+                            <div key={message.id}>
+                                {message.content}
+                            </div>
+                        ))}
+                    </Fragment>
+                ))}
+            </div>
         </div>
     )
 }
