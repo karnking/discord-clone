e533b2aae5b318a55a157ef5816e061f2a3082b6
karnking <karankotai9183@gmail.com>
2023-10-14 23:16:53 +0530

    messages showing image and file

--- components/chat/chat-items.tsx
+++ components/chat/chat-items.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { Member, Profile } from "@prisma/client";
+import { Member, MemberRole, Profile } from "@prisma/client";
 import { UserAvatar } from "../user-avatar";
 import { ActionTooltip } from "../action-tooltip";
+import { FileIcon, ShieldAlert, ShieldCheck } from "lucide-react";
+import Image from "next/image";
 
 interface ChatItemProps {
     id: string;
@@ -19,6 +21,12 @@ interface ChatItemProps {
     socketQuery: Record<string, string>;
 }
 
+const roleIconMap = {
+    [MemberRole.GUEST]: null,
+    [MemberRole.MODERATOR]: <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500"/>,
+    [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 ml-2 text-rose-400"/>,
+}
+
 export const ChatItem = ({
     id,
     content,
@@ -31,6 +39,15 @@ export const ChatItem = ({
     socketQuery,
     socketUrl
 }:ChatItemProps) => {
+    const fileType = fileUrl?.split(".").pop()
+    const isAdmin = currentMember.role === MemberRole.ADMIN;
+    const isModerator = currentMember.role === MemberRole.MODERATOR;
+    const isOwner = currentMember.id === member.id;
+    const canDeleteMessage = !deleted && (isAdmin || isModerator || isOwner)
+    const canEditMessage = !deleted && isOwner && !fileUrl;
+    const isPDF = fileType === "pdf" && fileUrl
+    const isImage = !isPDF && fileUrl
+
     return (
         <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
             <div className="group flex gap-x-2 items-center w-full">
@@ -42,10 +59,34 @@ export const ChatItem = ({
                         <div className="flex items-center">
                             <p className="font-semibold text-sm hover:underline cursor-pointer">{member.profile.name}</p>
                             <ActionTooltip label={member.role}>
-                                <p>Role</p>
+                                {roleIconMap[member.role]}
                             </ActionTooltip>
                         </div>
+                        <span className="text-xs text-zinc-500 dark:text-zinc-400">{timestamp}</span>
                     </div>
+                    {isImage && (
+                        <a 
+                            href={fileUrl}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="relative aspect-square rounded-md mt-2 overflow-hidden border flex items-center bg-secondary h-48 w-48"
+                        >
+                            <Image src={fileUrl} alt={content} fill className="object-cover"/>
+                        </a>
+                    )}
+                    {isPDF && (
+                        <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+                            <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+                            <a 
+                            href={fileUrl}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+                            >
+                            PDF File
+                            </a>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
