131ac907263380a4f9df97e2bc8096eec054c497
karnking <karankotai9183@gmail.com>
2023-10-17 13:11:46 +0530

    delete message function working

--- components/chat/chat-items.tsx
+++ components/chat/chat-items.tsx
@@ -20,6 +20,7 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatItemProps {
     id: string;
@@ -59,8 +60,8 @@ export const ChatItem = ({
     socketUrl
 }: ChatItemProps) => {
     const [isEditing, setIsEditing] = useState(false)
-    const [isDeleting, setIsDeleting] = useState(false)
-
+    const {onOpen} = useModal()
+    
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -206,7 +207,10 @@ export const ChatItem = ({
                         </ActionTooltip>
                     )}
                     <ActionTooltip label="Delete">
-                        <Trash className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-300 transition" />
+                        <Trash className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-300 transition" onClick={()=>onOpen("deleteMessage",{
+                            apiUrl: `${socketUrl}/${id}`,
+                            query: socketQuery
+                        })}/>
                     </ActionTooltip>
                 </div>
             )}
--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -12,6 +12,7 @@ import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
 import { EditChannellModal } from "@/components/modals/edit-channel-modal";
 import { MessageFileModal } from "../modals/message-file-modal";
+import { DeleteMessageModal } from "../modals/delete-message-modal";
 
 export const ModalProvider = () => {
     const [isMounted,setIsMounted] = useState(false);
@@ -30,6 +31,7 @@ export const ModalProvider = () => {
             <DeleteChannelModal />
             <EditChannellModal />
             <MessageFileModal />
+            <DeleteMessageModal />
         </>
     )
 }
