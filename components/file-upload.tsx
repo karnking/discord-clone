6a85bad15b835d9415dc07bf2aafb0f658f1303c
karnking <karankotai9183@gmail.com>
2023-10-18 17:32:21 +0530

    cheking type

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -5,19 +5,17 @@ import Image from "next/image";
 
 import { UploadDropzone } from "@/lib/uploadthing";
 
-import "@uploadthing/react/styles.css";
-
-interface FileUploadProps {
-  onChange: (url?: string) => void;
-  value: string;
-  endpoint: "messageFile" | "serverImage"
-}
+// interface FileUploadProps {
+//   onChange: (url?: string) => void;
+//   value: string;
+//   endpoint: "messageFile" | "serverImage"
+// }
 
 export const FileUpload = ({
   onChange,
   value,
   endpoint
-}: FileUploadProps) => {
+}:{onChange:any,value:any,endpoint:any}) => {
   const fileType = value?.split(".").pop();
 
   if (value && fileType !== "pdf") {
