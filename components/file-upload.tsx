cc9cc15208be91216218a83a8239ec561766bcda
karnking <karankotai9183@gmail.com>
2023-10-19 18:17:30 +0530

    final commit

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 import { UploadDropzone } from "@/lib/uploadthing";
@@ -6,18 +7,16 @@ import { UploadDropzone } from "@/lib/uploadthing";
 interface FileUploadProps {
   onChange: (url?: string) => void;
   value: string;
-  endpoint: "messageFile" | "serverImage"
+  endpoint: "messageFile" | "serverImage";
 }
 
-//@ts-nocheck
-//@ts-ignore
 export const FileUpload = ({
   onChange,
   value,
   endpoint
-}:FileUploadProps) => {
+}: FileUploadProps) => {
   const fileType = value?.split(".").pop();
-  
+
   if (value && fileType !== "pdf") {
     return (
       <div className="relative h-20 w-20">
@@ -26,12 +25,12 @@ export const FileUpload = ({
           src={value}
           alt="Upload"
           className="rounded-full"
-          />
+        />
         <button
           onClick={() => onChange("")}
           className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
           type="button"
-          >
+        >
           <X className="h-4 w-4" />
         </button>
       </div>
@@ -42,39 +41,35 @@ export const FileUpload = ({
     return (
       <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
         <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
-        <a 
+        <a
           href={value}
           target="_blank"
           rel="noopener noreferrer"
           className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
-          >
+        >
           {value}
         </a>
         <button
           onClick={() => onChange("")}
           className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
           type="button"
-          >
+        >
           <X className="h-4 w-4" />
         </button>
       </div>
     )
   }
-  
+
   return (
-    <
-    //@ts-ignore 
-    UploadDropzone
-    //@ts-ignore
-    endpoint={endpoint}
-    //@ts-ignore
-    onClientUploadComplete={(res) => {
-      onChange(res?.[0].url);
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
       }}
       onUploadError={(error: Error) => {
         console.log(error);
       }}
-      />
-      
-      )
-    }
+    />
+
+  )
+}
